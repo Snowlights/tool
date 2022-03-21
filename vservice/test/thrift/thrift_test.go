fa6bc7b93453d5241797ac05646ffb537c0309b0
zhangwei7125 <zhangwei7125@ipalfish.com>
2022-03-21 19:28:39 +0800

    chore:add thrift test

--- vservice/test/thrift/thrift_test.go
+++ vservice/test/thrift/thrift_test.go
@@ -1,1 +1,41 @@
 package thrift
+
+import (
+	"fmt"
+	"testing"
+	"vtool/idl/thrift/gen-go/thriftError"
+	"vtool/vservice/common"
+	"vtool/vservice/server"
+	. "vtool/vservice/test/thrift/thrift_protocol/gen-go/testService"
+)
+
+type helloServiceHandler struct {
+}
+
+func (h *helloServiceHandler) SayHello(req *SayHelloReq) (*SayHelloRes, error) {
+	return &SayHelloRes{
+		Data: &SayHelloData{Val: "this is val"},
+		ErrInfo: &thriftError.ErrInfo{
+			Code: 1,
+			Msg:  "this is error",
+		},
+	}, nil
+}
+
+type TestProcessor struct{}
+
+func (mp *TestProcessor) Engine() (string, interface{}) {
+
+	return "", NewTestServiceProcessor(new(helloServiceHandler))
+}
+
+func TestThriftServer(t *testing.T) {
+	err := server.ServService(map[common.ServiceType]common.Processor{
+		common.Thrift: &TestProcessor{},
+	})
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+}
